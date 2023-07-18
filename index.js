d85f4759b45a0f19c68be778cb85550fccb7acae
Riya <GlitchRemo@users.noreply.github.com>
2023-07-18 15:32:26 +0530

    refactor: added main function

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-const tasks = [
-  "Buy eggs from supermarket.",
-  "Finish Code Of Conduct training on campus.",
-  "Finish day 15 part 2 of Advent of Code problem.",
-  "Fill timesheet.",
-];
-
 const getTasksContainer = () => document.querySelector("#tasks-container");
 
 const createTaskElement = (taskText) => {
@@ -15,8 +8,17 @@ const createTaskElement = (taskText) => {
   return itemContainer;
 };
 
-window.onload = () => {
+const main = () => {
+  const tasks = [
+    "Buy eggs from supermarket.",
+    "Finish Code Of Conduct training on campus.",
+    "Finish day 15 part 2 of Advent of Code problem.",
+    "Fill timesheet.",
+  ];
+
   const tasksContainer = getTasksContainer();
   const taskElements = tasks.map(createTaskElement);
   taskElements.map((task) => tasksContainer.appendChild(task));
 };
+
+window.onload = main;
